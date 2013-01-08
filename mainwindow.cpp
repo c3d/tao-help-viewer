b0b4e4a1d872cb7b85d8df75cd7c982e377bafcc
Jerome Forissier <jerome@taodyne.com>
2013-01-08 16:39:16 +0100

    Update copyright notice from Nokia to Digia: (2) backport commit 6425d8b. Refs #2691.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,8 +3,7 @@
 ** Copyright (C) 2012 Taodyne SAS (contact@taodyne.com). All rights reserved.
 **
 ** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
+** Contact: http://www.qt-project.org/
 **
 ** This file is part of the Qt Assistant of the Qt Toolkit.
 **
@@ -32,6 +31,7 @@
 ** Other Usage
 ** Alternatively, this file may be used in accordance with the terms and
 ** conditions contained in a signed written agreement between you and Nokia.
+**
 **
 **
 **
